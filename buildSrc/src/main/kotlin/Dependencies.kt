e72e82fef9463ca1b8ce813bbefcae6c5a3190af
arhanashik <ashik.pstu.cse@gmail.com>
2020-09-25 22:50:08 +0900

    - kotlin-dsl updated. added "maven-publish"

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,8 +25,8 @@ object Config {
     }
 
     object Version {
-        const val code = 4
-        const val name = "1.1.2"
+        const val code = 5
+        const val name = "1.1.3"
     }
 
     const val androidJUnitRunner = "androidx.test.runner.AndroidJUnitRunner"
